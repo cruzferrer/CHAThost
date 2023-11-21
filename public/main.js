b08da64d05f99e7ab59665c407a5ac5e6aba83fa
cruzferrer <cruzricardofp@gmail.com>
2023-11-21 08:04:51 -0600

    new file

---
+++ public/main.js
@@ -0,0 +1,50 @@
+var socket = io.connect('http://localhost:8080', { 'forceNew': true });
+//Parte del cliente conectamos con localhost
+//escuchamos el evento messages
+// data tendr� el array de mensajes  que env�a el servidor
+socket.on('messages', function (data) {
+  console.log(data);
+  render(data);
+})
+// esta funci�n se encarga de pintar en el HTML los mensajes
+function render(data) {
+  var html = data.map(function (elem, index) {
+    return (`<div>
+              <strong>${elem.author}</strong>:
+              <em>${elem.text}</em>
+            </div>`);
+  }).join(" ");
+
+
+  document.getElementById('messages').innerHTML = html;
+
+
+}
+
+
+// main.js
+
+function addMessage(e) {
+  var message = {
+    author: document.getElementById('username').value,
+    text: document.getElementById('texto').value
+  };
+
+  var alerta = document.getElementById('alerta');
+
+  if (message.author == "" || message.text == "") {
+    alerta.style.display = 'block';
+  } else {
+    alerta.style.display = 'none';
+
+    socket.emit('new-message', message);
+    document.getElementById('texto').value = '';
+  }
+
+  return false;
+}
+
+
+
+
+
