5e2bbd1b0fa8c484091b63bc34f1112390bc277b
cruzferrer <cruzricardofp@gmail.com>
2023-11-21 18:48:28 -0600

    aaa

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ io.on('connection', function(socket) {
   socket.emit('messages', messages);
 
   socket.on('new-message', function(data) {
+    
     messages.push(data);
 
     io.sockets.emit('messages', messages);
