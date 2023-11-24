24d4f1d54d23d277e54160d678e2b7792d451400
cruzferrer <cruzricardofp@gmail.com>
2023-11-24 10:35:03 -0600

    app sin fron

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').Server(app);  
+var io = require('socket.io')(server , {
+  cors: {
+    origin: "*",
+  }
+});
+
+
+var messages = [{  
+  author: "A chatear!!!",
+  text: "CHAT",
+}];
+
+app.use(express.static('public'));
+// el socket escuchar� el evento new-message y los traera
+// en data con el m�todo push
+// para notificar a los clientes
+// para conectar en privado socket.emit
+// pero como es una sala de chat entonces
+//io.sockets.emit 
+io.on('connection', function(socket) {  
+  console.log('Alguien se ha conectado ');
+  socket.emit('messages', messages);
+
+  socket.on('new-message', function(data) {
+
+    messages.push(data);
+
+    io.sockets.emit('messages', messages);
+  });
+});
+
+
+
+
+server.listen(8080, function() {  
+  console.log("Servidor corriendo en https://chathostferrer2.onrender.com");
+});
+
+
--- main.js
+++ main.js
@@ -1,42 +1,98 @@
-var express = require('express');  
-var app = express();  
-var server = require('http').Server(app);  
-var io = require('socket.io')(server , {
-  cors: {
-    origin: "*",
+var socket = io.connect('https://chathostferrer2.onrender.com/');
+//Parte del cliente conectamos con localhost
+//escuchamos el evento messages
+// data tendr� el array de mensajes  que env�a el servidor
+socket.on('messages', function (data) {
+  console.log(data);
+  render(data);
+});
+// esta funci�n se encarga de pintar en el HTML los mensajes
+function render(data) {
+  var html = data.map(function (elem, index) {
+    return (`<div>
+              <strong>${elem.author}</strong>:
+              <em>${elem.text}</em>
+            </div>`);
+  }).join(" ");
+
+
+  document.getElementById('messages').innerHTML = html;
+
+
+};
+// main.js
+
+function enviar() {
+  var inombre = document.getElementById('user').value;
+  var userRegex = /^[a-zA-Z0-9 ]{1,20}$/;
+
+  if (inombre === '') {
+    alert('INGRESA ALGO');
+  } else {
+
+
+    if (!userRegex.test(inombre)) {
+      alert("El nombre no es válido. Debe tener entre 1 y 20 caracteres y los números del 1 al 9.");
+    }
+    else{
+      localStorage.setItem('nombre', inombre);
+      window.open("main.html");
+      render2(); 
+    }
+    
   }
-});
+}
+
+function render2() {
+  var user = localStorage.getItem('nombre');
+  var usernameInput = document.getElementById('username');
+  var nombreUsuarioSpan = document.getElementById('nombreUsuario');
+    nombreUsuarioSpan.textContent = user;
+    usernameInput.value = user;
+    
+  
+}
 
 
-var messages = [{  
-  id: "A chatear!!!",
-  text: "CHAT",
-}];
+function addMessage(e) {
 
-app.use(express.static('public'));
-// el socket escuchar� el evento new-message y los traera
-// en data con el m�todo push
-// para notificar a los clientes
-// para conectar en privado socket.emit
-// pero como es una sala de chat entonces
-//io.sockets.emit 
-io.on('connection', function(socket) {  
-  console.log('Alguien se ha conectado ');
-  socket.emit('messages', messages);
+  var USER = localStorage.getItem('nombre');
 
-  socket.on('new-message', function(data) {
+  var TEXT = document.getElementById('texto').value;
 
-    messages.push(data);
+  
+  var textRegex = /^[a-zA-Z0-9¿? ]{1,250}$/;
 
-    io.sockets.emit('messages', messages);
-  });
-});
+  
+    if (!textRegex.test(TEXT)) {
+      alert("El texto no es válido. Debe tener entre 1 y 250 caracteres y solo puede contener los caracteres ¿? y los números del 1 al 9.");
+    }
+    else {
+      var message = {
+        author: USER,
+        text: TEXT
+      };
+      var alerta = document.getElementById('alerta');
+      if (message.author == "" || message.text == "") {
+        alerta.style.display = 'block';
+
+      } else {
+        alerta.style.display = 'none';
+
+
+        socket.emit('new-message', message);
+        document.getElementById('texto').value = '';
+      }
+    }
+  
+
+  return false;
+};
 
 
 
 
-server.listen(8080, function() {  
-  console.log("Servidor corriendo en https://chathostferrer2.onrender.com");
-});
+
+
 
 
